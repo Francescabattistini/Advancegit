85f1be702ac544d0b5b484f17a96becd069b6e88
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-11-07 15:17:45 +0100

    Initial commit

---
+++ src/main/java/francescabattistini/Advancegit/AdvancegitApplication.java
@@ -0,0 +1,13 @@
+package francescabattistini.Advancegit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancegitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancegitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/francescabattistini/Advancegit/AdvancegitApplicationTests.java
@@ -0,0 +1,13 @@
+package francescabattistini.Advancegit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancegitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
